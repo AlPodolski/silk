b521642efee610173916d241f2edf1b34fcb3c83
aleksandrpodolskij <sanyapodolski@gmail.com>
2025-10-23 19:07:49 +0300

    правки

--- resources/views/new/cabinet/index.blade.php
+++ resources/views/new/cabinet/index.blade.php
@@ -24,7 +24,7 @@
             <div class="none-ankets">
                 <h2 class="none-ankets__title">
                     У вас нет добавленных </br>
-                    <img src="/intim-box/images/cabinet/notfound-ankets.png" alt="">анкет
+                    <img src="/images/notfound-ankets.png" alt="">анкет
                 </h2>
                 <a href="/cabinet/post/create" class="none-ankets__btn btn-main">
                     Добавить анкету
