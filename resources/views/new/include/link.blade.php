00a96f00881ba74e2d22dfcc3760dc57bd9b51ed
aleksandrpodolskij <sanyapodolski@gmail.com>
2025-11-08 11:08:18 +0200

    правки

--- resources/views/new/include/link.blade.php
+++ resources/views/new/include/link.blade.php
@@ -3,9 +3,6 @@
     <div class="links d-flex">
         @foreach($links as $item)
             @if($item['text'])
-                <a class="fast-link" href="/{{ $item['to'] }}">
-                    #{{ $item['text'] }}
-                </a>
                 <a href="/{{ $item['to'] }}" class="link">{{ $item['text'] }}</a>
             @endif
         @endforeach
